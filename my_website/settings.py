ebce779bc05d0900cbbff5c31c8e606edd4606fd
prayusgit <bhattarai.prayus@gmail.com>
2025-02-22 15:45:07 +0545

    My first project

--- my_website/settings.py
+++ my_website/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'h#g!)e7s3sh&-e5^jsqhu&cwl(&5$(n!-!@+1=bimi#q4#-@id'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
